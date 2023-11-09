0c47d63282765daa188cb0a93c4dc1510f7a54ae
Kadzora10 <adriankatama10@gmail.com>
2023-11-09 13:03:06 +0300

    first commit

---
+++ conditional stat.py
@@ -0,0 +1,32 @@
+# if conditions
+relationship = True
+if relationship: # condition
+    print("dating")
+
+# if ... else ..statement
+num = 30
+if num > 20:
+    print("Yes! the num is greater than 20")
+else:
+    print("No! It is less than 20")
+
+
+#if .... elif......else statement
+    
+Marks = 98
+if Marks >= 80:
+        print("A")
+elif Marks >= 70:
+        print("B")
+else:
+        print("fail")
+
+
+
+              
+
+
+
+
+    
+
